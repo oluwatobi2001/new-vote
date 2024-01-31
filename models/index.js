5286c36bd03b682d8eda4c4d76322be3fef16a09
oluwatobi2001 <tobilyn77@gmail.com>
2024-01-31 02:50:31 +0100

    final commit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const dotenv= require("dotenv")
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/vote');
+mongoose.connect(process.env.MONGO_URL);
 
 
 module.exports.User = require('./user');
